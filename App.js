c6d11d466fe31247148616f0c07a992d0f7a2f22
TheTower <thetoweruk@gmail.com>
2025-10-07 18:12:15 +0100

    fixup App.js to add console log and fix navigation stack

--- App.js
+++ App.js
@@ -1,26 +1,22 @@
 // App.js
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { AppProvider } from './src/state/AppState';
 
 import DashboardScreen from './src/screens/DashboardScreen';
 import AccountScreen from './src/screens/AccountScreen';
 import ReportsScreen from './src/screens/ReportsScreen';
 
-<Stack.Screen name="Account" component={AccountScreen} />
-const Stack = createNativeStackNavigator();
+const Stack = createNativeStackNavigator(); // <-- MUST have parentheses
 
 export default function App() {
-  console.log('[App] rendering');
   return (
-    <AppProvider>
-      <NavigationContainer>
-        <Stack.Navigator screenOptions={{ headerShown: false }} initialRouteName="Dashboard">
-          <Stack.Screen name="Dashboard" component={DashboardScreen} />
-          <Stack.Screen name="Account" component={AccountScreen} />
-          <Stack.Screen name="Reports" component={ReportsScreen} />
-        </Stack.Navigator>
-      </NavigationContainer>
-    </AppProvider>
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{ headerShown: false }} initialRouteName="Dashboard">
+        <Stack.Screen name="Dashboard" component={DashboardScreen} />
+        <Stack.Screen name="Account" component={AccountScreen} />
+        <Stack.Screen name="Reports" component={ReportsScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
+
