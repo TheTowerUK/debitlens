c1de878dc0c80ae58fb462f2383bce3d6095e8c6
TheTower <thetoweruk@gmail.com>
2025-10-21 13:52:45 +0100

    Test with Settings removed again

--- App.js
+++ App.js
@@ -6,7 +6,7 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import AppProvider from './src/state/AppState';
 
 // ---- choose your first screen here ----
-const START_SCREEN = 'Settings'; // change to 'Reports' or any route name below
+const START_SCREEN = 'Recurring'; // change to 'Reports' or any route name below
 // --------------------------------------
 
 const Stack = createNativeStackNavigator();
@@ -54,11 +54,6 @@ export default function App() {
             getComponent={() => require('./src/screens/RecurringScreen').default}
           />
 
-          <Stack.Screen
-            name="Settings"
-            options={{ title: 'Settings' }}
-            getComponent={() => require('./src/screens/SettingsScreen').default}
-          />
 
           {/* Example of a detail screen when you’re ready */}
           {/*
