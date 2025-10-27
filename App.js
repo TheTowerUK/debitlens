c93eb900ad6b1141a30aa2a10d23eb1ee7132f35
TheTower <thetoweruk@gmail.com>
2025-10-27 18:19:29 +0000

    Fix App.js

--- App.js
+++ App.js
@@ -1,19 +1,16 @@
 import React, { useEffect, useState } from 'react';
 import { View, Text } from 'react-native';
-import AppProvider from './src/state/AppState';
+import AppProvider from './src/state/AppProvider';
+
 
 
 export default function App() {
-  const [count, setCount] = useState(0);
-
-  useEffect(() => {
-    const timer = setTimeout(() => setCount(c => c + 1), 1000);
-    return () => clearTimeout(timer);
-  }, [count]);
-
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Count: {count}</Text>
-    </View>
+    <AppProvider>
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text>Count: {count}</Text>
+      </View>
+    </AppProvider>
   );
 }
+
