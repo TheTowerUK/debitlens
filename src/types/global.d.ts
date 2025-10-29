b3b9266d4c8a0b2dd18388e164276ace4c563fe3
TheTower <thetoweruk@gmail.com>
2025-10-29 10:56:05 +0000

    Fix Global.d.ts

--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -1,15 +1,17 @@
 export {};
 
 declare global {
-  // Partial React Native ErrorUtils contract
-  var ErrorUtils: {
-    getGlobalHandler?: () => (error: any, isFatal?: boolean) => void;
+  // Partial ErrorUtils contract used by React Native / Expo apps
+  interface ErrorUtils {
+    getGlobalHandler?: () => ((error: any, isFatal?: boolean) => void) | undefined;
     setGlobalHandler?: (handler: (error: any, isFatal?: boolean) => void) => void;
-  } | undefined;
+  }
 
+  var ErrorUtils: ErrorUtils | undefined;
+
+  // guard used by your installGlobalHandlers implementation
   var __GLOBAL_HANDLER_SET__: boolean | undefined;
 
-  interface GlobalEventHandlers {
-    onunhandledrejection?: ((event: any) => void) | undefined;
-  }
+  // optional: runtime handler for unhandled promise rejections
+  var onunhandledrejection: ((event: any) => void) | undefined;
 }
