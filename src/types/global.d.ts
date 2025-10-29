1d5890c648e0167d4867b67dab8596ddbef76911
TheTower <thetoweruk@gmail.com>
2025-10-29 08:48:44 +0000

    Updated Global.d.ts and Package.json

--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -1,11 +1,15 @@
 export {};
 
 declare global {
-  // React Native ErrorUtils (partial)
+  // Partial React Native ErrorUtils contract
   var ErrorUtils: {
     getGlobalHandler?: () => (error: any, isFatal?: boolean) => void;
     setGlobalHandler?: (handler: (error: any, isFatal?: boolean) => void) => void;
   } | undefined;
 
   var __GLOBAL_HANDLER_SET__: boolean | undefined;
+
+  interface GlobalEventHandlers {
+    onunhandledrejection?: ((event: any) => void) | undefined;
+  }
 }
