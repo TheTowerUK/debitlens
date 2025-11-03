c1feeb79ce5fb82babd309983fbe35204dba7dea
TheTower <thetoweruk@gmail.com>
2025-11-03 20:37:12 +0000

    Updated App.tsx

--- App.tsx
+++ App.tsx
@@ -2,14 +2,31 @@
 import 'react-native-gesture-handler';
 import React from 'react';
 import { View, Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
 import AppProvider from './src/state/AppProvider';
+
+const Stack = createNativeStackNavigator();
+
+function DummyScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Dummy screen OK</Text>
+    </View>
+  );
+}
 
 export default function App() {
   return (
     <AppProvider>
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>AppProvider + root OK</Text>
-      </View>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Dummy">
+          <Stack.Screen name="Dummy" component={DummyScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
     </AppProvider>
   );
 }
+
+
