d6fbfeef1170c141198ffad237b8373ff5bf11ea
TheTower <thetoweruk@gmail.com>
2025-11-28 16:27:54 +0000

    <Text> update in BudgetScreen

--- App.tsx
+++ App.tsx
@@ -3,16 +3,17 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import RootNavigator from './src/navigations/RootNavigator';
 import { AppProvider } from './src/state/AppContext';
-import NotificationBootstrapper from './src/notifications/NotificationBootstrapper';
+// import NotificationBootstrapper from './src/notifications/NotificationBootstrapper';
 
 const App: React.FC = () => (
   <AppProvider>
-    <NotificationBootstrapper />
+    {/* Notifications disabled while we debug startup */}
+    {/* <NotificationBootstrapper /> */}
+
     <NavigationContainer>
       <RootNavigator />
     </NavigationContainer>
   </AppProvider>
 );
-
 
 export default App;
