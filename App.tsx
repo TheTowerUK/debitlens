5c5cd9dcc61b568c48c862f3f54036926c79fd97
TheTower <thetoweruk@gmail.com>
2025-11-28 11:38:03 +0000

    Temp fix for Bootup

--- App.tsx
+++ App.tsx
@@ -3,11 +3,12 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import RootNavigator from './src/navigations/RootNavigator';
 import { AppProvider } from './src/state/AppContext';
-import NotificationBootstrapper from './src/notifications/NotificationBootstrapper'; // 👈 new path
+// import NotificationBootstrapper from './src/notifications/bootstrapper'; // ❌ temporarily
 
 const App: React.FC = () => (
   <AppProvider>
-    <NotificationBootstrapper />
+    {/* <NotificationBootstrapper />  // ❌ temporarily remove */}
+
     <NavigationContainer>
       <RootNavigator />
     </NavigationContainer>
