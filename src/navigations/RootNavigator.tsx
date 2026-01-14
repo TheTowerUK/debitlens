d8b2e21957af040a87604c76473f489392e9928b
TheTower <thetoweruk@gmail.com>
2026-01-14 08:00:19 +0000

    HeaderShown = True

--- src/navigations/RootNavigator.tsx
+++ src/navigations/RootNavigator.tsx
@@ -71,7 +71,7 @@ export default function RootNavigator() {
       id={undefined as any}
       initialRouteName="Login"
       screenOptions={{
-        headerShown: false,
+        headerShown: true,
       }}
     >
       <Stack.Screen name="Login" component={SplashAuthScreen} />
